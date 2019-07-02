f014f5ebcc68d30f0646593d78f3aae56507264e
nlds <nldaseul@gmail.com>
2019-07-02 11:33:31 +0900

    Add User, ChaatRoom, Message models in api

--- api/models.py
+++ api/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
+class User(models.Model):
+    nickname = models.CharField(max_length=225)
+    email = models.CharField(max_length=225)
 
-class ChatList(models.Model):
-    room = models.CharField(max_length=225)
-    created_at = models.DateTimeField(auto_now_add=True)
+
+class ChatRoom(models.Model):
+    user1 = models.ForeignKey(User, related_name='user1', on_delete=models.CASCADE)
+    user2 = models.ForeignKey(User, related_name='user2', on_delete=models.CASCADE)
+    roomname = models.CharField(max_length=225)
+    channel_url = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Message(models.Model):
+    channel = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, related_name='sender', on_delete=models.CASCADE)
+    message = models.TextField()
+    message_id = models.CharField(max_length=225)
