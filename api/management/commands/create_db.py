18cef697bc29bc406c8e7071a76e868197d3ab16
nlds <nldaseul@gmail.com>
2019-07-08 13:02:23 +0900

    Add create_db.py for connecting AWS rds migration

--- api/management/commands/create_db.py
+++ api/management/commands/create_db.py
@@ -5,8 +5,8 @@ import MySQLdb
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
-rds_host = 'sendbird-cluster.cluster-c79s4ys8xz06.ap-northeast-2.rds.amazonaws.com'
-db_name = 'mysql'
+rds_host = 'sendbird-cluster.cluster-csawlbhmc6s3.us-east-1.rds.amazonaws.com'
+db_name = 'sendbird'
 user_name = 'member'
 password = 'tsod2527'
 port = 3306
@@ -21,20 +21,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print('Starting db creation')
         try:
-            db = MySQLdb.connect(
-                host=rds_host, user=user_name,
-                password=password, db='mysql', connect_timeout=5
-            )
+            db = MySQLdb.connect(host=rds_host, user=user_name,
+                                 password=password, db="mysql", connect_timeout=5)
             c = db.cursor()
             print("connected to db server")
-            c.execute("""CREATE DATABASE mysql;""")
+            c.execute("""CREATE DATABASE pollsdb;""")
             c.execute(
-                """GRANT ALL PRIVILEGES ON db_name.* TO 'member' IDENTIFIED BY 'member';"""
-            )
+                """GRANT ALL PRIVILEGES ON db_name.* TO 'polls_admin' IDENTIFIED BY 'pollsadmin';""")
             c.close()
             print("closed db connection")
         except:
             logger.error(
-                "ERROR: Unexpected error : Could not connect to Mysql instance." 
-            )
+                "ERROR: Unexpected error: Could not connect to MySql instance.")
             sys.exit()
